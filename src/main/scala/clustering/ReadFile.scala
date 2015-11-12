64708ac71106bfce030253fcd896bd6c8e26d076
Vladimir Smida <smida@apple.com>
2015-11-12 16:15:26 +0100

    Changed input to yelp data

--- src/main/scala/clustering/ReadFile.scala
+++ src/main/scala/clustering/ReadFile.scala
@@ -4,11 +4,14 @@ import com.twitter.scalding._
 
 class Main( args: Args ) extends Job( args )
 {
+  val input = Tsv( args( "input" ), ('city, 'review_count, 'name, 'neighborhoods, 'type, 'business_id, 'full_address, 'state, 'hours, 'longitude, 'stars, 'latitude, 'attributes, 'open, 'categories))
+  val output =Tsv( args( "output" ) )
 
-  val input = Tsv( args( "input" ) )
-  val output = Tsv( args( "output" ) )
+  val pipe = input.read
 
-  val inputFields = ('city, 'review_count, 'name, 'neighborhoods, 'type, 'business_id, 'full_address, 'state, 'hours, 'longitude, 'stars, 'latitude, 'attributes, 'open, 'categories)
-  input.read.write( output )
+    pipe.project('latitude, 'longitude)
+      .write( output )
+
+
 
 }
